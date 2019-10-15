3f3c12aea621020a44c9b7ba3aa54db4532f5771
Anton Truong <anton.truong@axoom.com>
2019-10-15 16:29:36 +0200

    Adds test for number points in polyline element

--- src/app/polyline/polyline.component.spec.ts
+++ src/app/polyline/polyline.component.spec.ts
@@ -33,4 +33,21 @@ describe('PolylineComponent', () => {
     expect(svgDebugElement).toBeDefined();
     expect(polylineDebugElement).toBeDefined();
   });
+
+  it('Expect number of points in the polyline element', () => {
+    // Creating SVGPoints
+    const svgPoints = [];
+    const svg: SVGSVGElement = svgDebugElement.nativeElement
+    const point1 = svg.createSVGPoint();
+    point1.x = 100;
+    point1.y = 100;
+    const point2 = svg.createSVGPoint();
+    point2.x = 100;
+    point2.y = 100;
+    svgPoints.push(point1, point2);
+    component.points = svgPoints;
+    const polyline: SVGPolylineElement = polylineDebugElement.nativeElement;
+    // Important part of this test
+    expect(polyline.points.length).toBe(2);
+  });
 });
