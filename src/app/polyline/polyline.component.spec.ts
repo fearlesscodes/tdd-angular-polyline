e72dfd9e746a2c4a59d7a32d4b71e04e926dcc55
Anton Truong <antont86@gmail.com>
2019-10-27 19:24:42 +0100

    Removes old test using SVGPoints outside the component

--- src/app/polyline/polyline.component.spec.ts
+++ src/app/polyline/polyline.component.spec.ts
@@ -34,26 +34,7 @@ describe('PolylineComponent', () => {
     expect(polylineDebugElement).toBeDefined();
   });
 
-  it('Expect number of points in the polyline element', async(() => {
-    // Creating SVGPoints
-    const svgPoints = [];
-    const svg: SVGSVGElement = svgDebugElement.nativeElement;
-    const point1 = svg.createSVGPoint();
-    point1.x = 100;
-    point1.y = 100;
-    const point2 = svg.createSVGPoint();
-    point2.x = 100;
-    point2.y = 100;
-    svgPoints.push(point1, point2);
-    component.points = svgPoints;
-    component.ngOnInit();
-
-    const polyline: SVGPolylineElement = polylineDebugElement.nativeElement;
-    // Important part of this test
-    expect(polyline.points.length).toBe(2);
-  }));
-
-  fit('Expect number of points in the polyline element using default DOMPoint objects', async(() => {
+  it('Expect number of points in the polyline element using default DOMPoint objects', async(() => {
     // Creating DomPoints
     const domPoints = [];
     const point1 = new DOMPoint(100, 100);
